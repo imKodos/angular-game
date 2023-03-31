6e164e6817a3af926af51d2977818c24ed17c1a9
Ken <notnek4@gmail.com>
2023-03-31 16:48:42 -0400

    initial game state

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import Phaser from 'phaser'; //import Phaser
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.css']
+})
+
+export class GameComponent implements OnInit {
+  //declare phaserGame variable, the ! is needed for it to be valid code in Angular, we just have to make sure we initialize it in ngOnInit
+  phaserGame!: Phaser.Game; 
+  config: Phaser.Types.Core.GameConfig;
+  constructor() {
+    this.config = {
+      type: Phaser.AUTO,
+      height: 600,
+      width: 800,
+      scene: [ MainScene ],
+      parent: 'gameContainer',
+      title: "Grim RPG",
+      backgroundColor: "#18216D",
+      physics: {
+        default: 'matter',
+        matter: {
+           debug:true,
+           gravity: { y: 0 }
+        }
+      },
+    };
+  }
+  ngOnInit() {
+    this.phaserGame = new Phaser.Game(this.config);
+  }
+}
+
+class MainScene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'MainScene' });
+  }
+  create() {
+    console.log('create method');
+  }
+  preload() {
+    console.log('preload method');
+  }
+  override update(){
+    console.log('update method');
+  }
+}
